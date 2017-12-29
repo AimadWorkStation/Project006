194a62cab3132f3bace32a127b2723c150c149d9
EmadEddin <aimad.tchad@gmail.com>
2017-12-29 18:58:07 +0000

    at this point i have created the structure and adding labreries and tools to use like bootstrap and fontowsome also managing to start with the header and footer in the admin side

---
+++ admin/index.php
@@ -0,0 +1,10 @@
+<?php 
+	include 'includes/templates/header.inc';
+ ?>
+	
+	Welcom to index
+	<button class="btn btn-danger btn-block">Test bootstrap</button>
+
+ <?php 
+	include 'includes/templates/footer.inc';
+ ?>
