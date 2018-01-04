0881952189a81026b0eab29789d652d734ec73d4
EmadEddin <aimad.tchad@gmail.com>
2018-01-04 02:12:36 +0000

    So i have have added the manage page with a reponsive bootsrap table, i deceded before to work at this project with only pure PHP but i think that i will add Ajax to handle the data requests

--- admin/users.php
+++ admin/users.php
@@ -11,10 +11,168 @@
 
 		switch ($do) {
 			case 'Manage':
+			// ********** start users page ********
+			
+			$stmt = $con->prepare('select * from users where GroupID != 1');
+			$stmt -> execute();
+
+		?>
+				<!-- start manage users page -->
+				<h1 class="text-center">Manage</h1>
+				<div class='container'>
+					<div class="table-responsive">
+						
+						<table class="mainTable table table-bordered text-center">
+							<tr>
+								<td>#</td>
+								<td>username</td>
+								<td>Email</td>
+								<td>Full name</td>
+								<td>Adresse</td>
+								<td>City</td>
+								<td>Registred Date</td>
+								<td>Controle</td>
+							</tr>
+							<?php 
+
+								while ($row = $stmt->fetch()) { 
+
+							 ?>
+								<tr>
+									<td><?php echo $row['UserID']; ?></td>
+									<td><?php echo $row['UserName']; ?></td>
+									<td><?php echo $row['Email']; ?></td>
+									<td><?php echo $row['FullName']; ?></td>
+									<td><?php echo $row['Adresse']; ?></td>
+									<td><?php echo $row['City']; ?></td>
+									<td></td>
+									<td>
+										<a href=""><i class="fa fa-trash-o fa-2x fa-fw"></i></a>
+			      						<a href="users.php?do=Edit&userid=<?php  echo $row['UserID'];  ?>"><i class="fa fa-pencil-square-o fa-2x fa-fw"></i></a>
+									</td>
+								</tr>
+							<?php } ?>
+
+						</table>
+
+					</div>
+					<a href='users.php?do=Add' class="btn btn-primary"><i class="fa fa-plus"></i> Add new user</a>
+				
+
+				</div>
+				<!-- end manage users page -->
+
+		<?php	// ********** end add case ******* 
+				break;
+
+			case 'Add':
 			// ********** start Add users page ********
-				echo "Users";
+			
+				?>		
+				
+				<!-- start html code -->
+					<h1 class="text-center">Add User</h1>
+					<div class="container">
+								<form action="?do=insert" method="POST" class="w-75 m-auto">
+									  <div class="form-row">
+									  	<div class="form-group col-md-12">
+									      <label for="username">Username</label>
+									      <input type="text" class="form-control" name="username" placeholder="Username" required>
+									    </div>
+									    <div class="form-group col-md-6">
+									      <label for="userfullname">User Full Name</label>
+									      <input type="text" class="form-control" name="fullname" placeholder="User Full Name" required>
+									    </div>
+									    <div class="form-group col-md-6">
+									      <label for="email">Email</label>
+									      <input type="email" class="form-control" name="email" placeholder="Email" required>
+									    </div>
+									  </div>
+									  <div class="form-group col-md-6">
+									    <label for="password">New Password</label>
+									    <input type="password" class="form-control" name="pass" placeholder="Password" required>
+									  </div>
+									  <div class="form-row">
+									    <div class="form-group col-md-6">
+									      <label for="adresse">Adresse</label>
+									      <input type="text" class="form-control" name="address" placeholder="Your address">
+									    </div>
+									    <div class="form-group col-md-4">
+									      <label for="city">City</label>
+									      <input type="text" class="form-control" name="city" placeholder="Your city">
+									    </div>
+									  </div>
+									  <div class="form-group">
+									    <div class="form-check">
+									  </div>
+									  <button type="submit" class="btn btn-primary">Add User</button>
+
+									  <!-- if succeeded or not show message-->
+									  <?php if(isset($succeed)){
+									  	echo $succeed;	;
+									  } ?>
+									</form>
+						</div>
+
+				<!-- end html code -->
+
+			<?php
 
 			// ********** end add case *******
+				break;
+
+			case 'insert':
+				
+				//start insert code for add form
+				 
+				
+
+
+								//Testing if request method is Post for button submit 
+				if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+					$username = $_POST['username'];
+					$fullname = $_POST['fullname'];
+					$email = $_POST['email'];
+					$pass = sha1($_POST['pass']);
+					$address = $_POST['address'];
+					$city = $_POST['city'];
+
+
+					try {
+					
+						$stmt = $con -> prepare("insert into users(username,password,FullName, Email,adresse, city) values(?,?,?,?,?,?)");
+						$stmt -> execute(array($username,$pass,$fullname,$email,$address,$city));
+						
+						echo $succeed = "<div class='alert alert-success alert-dismissible fade show w-50 h-25 mt-5 ml-auto mr-auto' role='alert'>
+									  				<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+												    <span aria-hidden='true'>&times;</span>
+												  	</button>
+												  <strong>Well done!</strong> user added successfully.
+												</div>";
+
+					} catch (Exception $e) {
+						echo $wrongpass = "<div class='alert alert-warning alert-dismissible fade show' 	role='alert'>
+									  				<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+												    <span aria-hidden='true'>&times;</span>
+												  	</button>
+												  <strong>Oh Snap!</strong> Somthing Goes wrong" . $e -> $e.getMessage() . " !!.
+												</div>";
+					}
+				}
+				else
+				{
+					echo $wrongpass = "<div class='alert alert-danger alert-dismissible fade show w-50 mt-5 ml-auto mr-auto' 	role='alert'>
+									  				<button type='button' class='close' data-dismiss='alert' aria-label='Close'>
+												    <span aria-hidden='true'>&times;</span>
+												  	</button>
+												  <strong>Oh Snap!</strong> U cant access this page !!.
+												</div>";
+				}
+
+
+
+				//end insert code for add form
 				break;
 			
 			case 'Edit': 
@@ -24,12 +182,12 @@
 				$userId = (isset($_GET['userid']) && is_numeric($_GET['userid'])) ? intval($_GET['userid']) : 0;
 
 				//make sur that noOne has the possibility to bring info only by changing id at the GET Resuest but the username in the session also
-				$stmt = $con->prepare("Select * from users where username = ? and userid = ? LIMIT 1");
+				$stmt = $con->prepare("Select * from users where userid = ? LIMIT 1");
 
-				$stmt -> execute(array($_SESSION['Username'],$userId));
+				$stmt -> execute(array($userId));
 				$row = $stmt -> fetch();
 				$count = $stmt -> rowCount();
-
+				
 
 				//Testing if request method is Post for button submit 
 				if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -43,7 +201,7 @@
 					$Ucity = $_POST['city'];
 
 					$stmt = $con -> prepare("Select * from users where userid = ?");
-					$stmt -> execute(array($_SESSION['ID']));
+					$stmt -> execute(array($userId));
 					$row = $stmt -> fetch();
 
 					// verify if input is empty set the old value
