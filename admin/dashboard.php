6ce4835f072795bc981173e604e307680ebbe4a9
EmadEddin <aimad.tchad@gmail.com>
2018-01-07 03:10:10 +0000

    i have prepare the dashboard page to link it with the database

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,7 +5,64 @@
 		$pageTitle = 'Dashboard';
 		include 'init.php';
 
-		echo "main dashbord";
+		//start dashboard page
+		?>
+		<div class="dashboard">
+			<div class="container dashStat text-center">
+				
+				<h1>Dashboad</h1>
+
+				<div class="row">
+					<div class="col-md-3 stats">
+						<div class="stat">Total users
+						<span>323432</span>
+						</div>
+						
+					</div>
+					<div class="col-md-3 stats">
+						<div class="stat">Pending users
+						<span>323432</span>
+					</div>
+						
+					</div>
+					<div class="col-md-3 stats">
+						<div class="stat">Total Items<span>323432</span>
+					</div>
+						
+					</div>
+					<div class="col-md-3 stats">
+						<div class="stat">Total Comments<span>323432</span>
+					</div>
+						
+					</div>
+				</div>
+
+			</div>
+			<div class="container dashLatest">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="panel panel-default">
+						  <div class="panel-heading"><i class="fa fa-users"></i>  Latest Registred users</div>
+						  <div class="panel-body">
+						    Panel content
+						  </div>
+						</div>
+					</div>
+
+					<div class="col-sm-6">
+						<div class="panel panel-default">
+						  <div class="panel-heading"><i class="fa fa-ticket"></i>  Latest Registred users</div>
+						  <div class="panel-body">
+						    Panel content
+						  </div>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<?php 
+		//end dashboard page 
 
 		include $tpl. 'footer.inc';
 	}
